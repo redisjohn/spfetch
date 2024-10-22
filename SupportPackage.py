c579ca412d32ff6d43b9660b63f207f8e57f5edd
john isaac <john.isaac@gridsmart.com>
2024-10-22 17:38:01 -0400

    lite version

--- SupportPackage.py
+++ SupportPackage.py
@@ -1,10 +1,9 @@
 import os
 from datetime import datetime
 import requests
+import json
 
 class SupportPackage:
-
-    
 
     @staticmethod
     def get_fname(fqdn,path):
@@ -15,6 +14,72 @@ class SupportPackage:
                 os.makedirs(path)
             return os.path.join(path,fname)
 
+    @staticmethod
+    def tablulate_bdb_info(response):
+
+        bdb_json = json.loads(response)
+
+        #sort by uid
+        bdb_names = sorted(bdb_json, key=lambda x: x['uid'])
+
+        # column width for tablular format
+        name_width = max(len(bdb['name']) for bdb in bdb_names) + 2
+        uid_width = max(len(str(bdb['uid'])) for bdb in bdb_names) + 2
+        version_width = max(len(str(bdb['version'])) for bdb in bdb_names) + 2
+        shards_width = max(len(str(bdb['shards_count'])) for bdb in bdb_names) + 2
+
+        print(f"{'Id'.ljust(uid_width)} {'Name'.ljust(name_width)} {'Version'.ljust(version_width)} {'Shards'.ljust(shards_width)}")
+        print('-' * (uid_width + name_width + version_width + shards_width + 6))
+
+        for bdb in bdb_names:
+            print(f"{str(bdb['uid']).ljust(uid_width)}  {str(bdb['name']).ljust(name_width)} {str(bdb['version']).ljust(version_width)} {str(bdb['shards_count']).ljust(shards_width)}")
+
+    def deserialize_bdb_info(fqdn,response):
+
+        bdb_json = json.loads(response)
+        for bdb in bdb_json:
+            bdb['cluster'] = fqdn
+            if (bdb['slave_ha']):
+                bdb['shards_count'] *= 2
+        #sort by uid
+        bdb_names = sorted(bdb_json, key=lambda x: (x['cluster'], x['uid']))
+        for bdb in bdb_names:
+            print(json.dumps(bdb, indent=4)) 
+
+
+    @staticmethod
+    def get_bdb_names(fqdn,username,password):
+        try:
+            requests.packages.urllib3.disable_warnings()
+            download_url = "https://" + fqdn + ":9443/v1/bdbs?fields=uid,name,version,shards_count,slave_ha"                 
+            response = requests.get(download_url, auth=(username, password), verify=False)            
+            response.raise_for_status()
+            return response.text
+        except requests.exceptions.RequestException as e:
+            print(f"Error Getting Database Names for {fqdn}: {e}")                  
+
+    @staticmethod
+    def deserialize_license_info(fqdn,response):
+        license = json.loads(response)
+        output = {}
+        output['cluster'] = fqdn 
+        output['shards_limit'] = license['shards_limit']
+        output['ram_shards'] = license['ram_shards_in_use']
+        output['flash_shards'] = license['flash_shards_in_use']
+        print(json.dumps(output, indent=4)) 
+
+    @staticmethod
+    def get_license_info(fqdn,username,password):
+        try:
+            requests.packages.urllib3.disable_warnings()
+            download_url = "https://" + fqdn + ":9443/v1/license"                 
+            response = requests.get(download_url, auth=(username, password), verify=False)            
+            response.raise_for_status()
+            return response.text
+        except requests.exceptions.RequestException as e:
+            print(f"Error License Info for {fqdn}: {e}")                  
+        
+    
     @staticmethod
     def download_package(fqdn, username, password,path):
         try:
--- spfetch.py
+++ spfetch.py
@@ -2,8 +2,10 @@
 import argparse
 from SupportPackage import SupportPackage
 from CredentialVault import CredentialVault
+from Fqdns import FQDNs
 import os
 import re
+import Fqdns
 
 def sort_and_keep_latest_files(folder_path, file_prefix, n):
     # Get a list of all files in the folder
@@ -33,8 +35,18 @@ def main():
     parser.add_argument("--pwd", help="Password for authentication")
     parser.add_argument("--path", help="Folder path for saving package")
     parser.add_argument("--keep",help="Number of Packages to keep")
+    parser.add_argument("--list",action='store_true',help="List Databases Id and Names")
+    parser.add_argument("--license",action='store_true',help="List Databases Id and Names")
+    parser.add_argument("--json",action='store_true',help="Format Database Output List in Json")
+    
     args = parser.parse_args()
-           
+    user = ''
+    pwd = '' 
+
+    if args.fqdn == 'all':
+        fqdns = FQDNs.get()
+        print(fqdns) 
+        os._exit(0)
     if args.path:
         path = args.path
     else:
@@ -53,14 +65,28 @@ def main():
         except Exception as e:
             print(f"Fatal Error: {str(e)}")
             exit(-1)
-    try: 
-        SupportPackage.download_package(args.fqdn, user,pwd,path) 
-        if (args.keep is not None):
-            sort_and_keep_latest_files(path,args.fqdn,int(args.keep))    
-       
-    except Exception as e:
-        print(f"Fatal Error: {str(e)}")
-        exit(-1)
+
+    if (args.list):
+        try: 
+            response = SupportPackage.get_bdb_names(args.fqdn,user,pwd)
+            if (args.json):
+                SupportPackage.deserialize_bdb_info(args.fqdn,response)
+            else:
+                SupportPackage.tablulate_bdb_info(response)
+        except Exception as e:
+            print(f"Error Getting Database Names:{str(e)}")
+    elif (args.license):
+        response = SupportPackage.get_license_info(args.fqdn,user,pwd)
+        SupportPackage.deserialize_license_info(args.fqdn,response)
+    else:
+        try: 
+            SupportPackage.download_package(args.fqdn, user,pwd,path) 
+            if (args.keep is not None):
+                sort_and_keep_latest_files(path,args.fqdn,int(args.keep))    
+        
+        except Exception as e:
+            print(f"Fatal Error: {str(e)}")
+            exit(-1)
 
 
 if __name__ == "__main__":
